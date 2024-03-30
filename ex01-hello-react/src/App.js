f2d6f3f9b5247e336d12a4b6667774ab09d63d10
Gaurab Kumar <progaurab@gmail.com>
2024-03-30 08:39:04 -0700

    ex01

---
+++ ex01-hello-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello React!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
